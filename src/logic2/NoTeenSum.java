f8c14f8528a226926fb091ed21962db5fe1fa840
James Wallace <jawallace0392@gmail.com>
2022-03-30 01:35:13 -0400

    added task

--- src/logic2/NoTeenSum.java
+++ src/logic2/NoTeenSum.java
@@ -1,5 +1,12 @@
 package logic2;
 
+/*Given 3 int values, a b c, return their sum. However, if any of the values is a teen
+-- in the range 13..19 inclusive -- then that value counts as 0, except 15 and 16 do not count as a teens.
+Write a separate helper "public int fixTeen(int n) {
+"that takes in an int value and returns that value fixed for the teen rule.
+In this way, you avoid repeating the teen code 3 times (i.e. "decomposition").
+Define the helper below and at the same indent level as the main noTeenSum().*/
+
 public class NoTeenSum {
     public int noTeenSum(int a, int b, int c) {
         return fixTeen(a) + fixTeen(b) + fixTeen(c);
