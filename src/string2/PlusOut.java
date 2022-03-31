336342ffb5a4982706cd50a35ceae257419811cf
James Wallace <jawallace0392@gmail.com>
2022-03-31 15:09:02 -0400

    not best solution might come back

--- src/string2/PlusOut.java
+++ src/string2/PlusOut.java
@@ -1,26 +1,40 @@
 package string2;
 
-/*Given a string and a non-empty word string, return a version of the original String where all chars have been replaced by pluses ("+"), except for appearances of the word string which are preserved unchanged.*/
+/*Given a string and a non-empty word string, return a version of the original String
+where all chars have been replaced by pluses ("+"), except for appearances of the word
+string which are preserved unchanged.*/
 
 public class PlusOut {
     public String plusOut(String str, String word) {
-        for (int i = 0; i < str.length() - word.length(); i++) {
-            if (!str.substring(i , i + word.length()).equals(word)) {
-                str = str.replaceFirst(str.charAt(i) + "", "+");
+        int count = 0;
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == '+') {
+                count++;
+                break;
+            }
+        }
+        String send = "";
+        if (count != 0) {
+            int x = str.length();
+            while (x > 0) {
+                send += "+";
+                x--;
+            }
+            return send;
+        }
+        str = str.replaceAll(word, "_" + word + "_");
+        String[] arr = str.split("_");
+        for (String element : arr) {
+            if (element.equals(word)) {
+                send += word;
             } else {
-                i += word.length() - 1;
+                int x = element.length();
+                while (x > 0) {
+                    send += "+";
+                    x--;
+                }
             }
         }
-        String add = "";
-        if (str.charAt(str.length() - 1) != '+' && !str.substring(str.length() - word.length()).equals(word)) {
-            for (int i = str.length() - word.length(); i < str.length(); i++) {
-                add += "+";
-            }
-        }
-        if (str.charAt(str.length() - 1) != '+' && !str.substring(str.length() - word.length()).equals(word)) {
-            str = str.substring(0 , str.length() - word.length());
-        }
-
-        return str + add;
+        return send;
     }
 }
