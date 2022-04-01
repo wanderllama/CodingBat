a5b65b307acb7f0d349dfee758bf6644a5bb2508
James Wallace <jawallace0392@gmail.com>
2022-04-01 09:29:35 -0400

    finished

--- src/string2/WordEnds.java
+++ src/string2/WordEnds.java
@@ -5,35 +5,31 @@ just before and just after every appearance of the word in the string. Ignore
 cases where there is no char before or after the word, and a char may be included
 twice if it is between two words.*/
 
+import java.util.ArrayList;
+
 public class WordEnds {
-
-    public static void main(String[] args) {
-        String str = "abc1xyz11";
-
-        String word = "1";
-        str = str.replaceAll(word, " " + word + " ");
-        String[] arr = str.split(" ");
-        String send = "";
-
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i].isEmpty()) {
-                continue;
-            } else if (!arr[i].equals(word)) {
-                continue;
-            } else if ((i == 0 || arr[i - 1].isEmpty()) && arr[i].equals(word)) {
-                send += arr[i + 1].charAt(0);
-                System.out.println("send = " + send);
-            } else if (i == arr.length - 1 && arr[i].equals(word)) {
-                send += arr[i - 1].charAt(arr[i - 1].length() - 1);
-            } else if (arr[i].equals(word)) {
-                send += arr[i - 1].charAt(arr[i - 1].length() - 1);
-                if (arr[i + 1].isEmpty()) {
-                    continue;
-                } else {
-                    send += arr[i + 1].charAt(0);
-                }
+    public String wordEnds(String str, String word) {
+        if(str.equals(word)) {
+            return "";
+        }
+        String[] arrs = str.replaceAll(word , " " + word + " ").split(" ");
+        ArrayList<String> arr = new ArrayList<>();
+        for (String element : arrs) {
+            if (!element.isEmpty()) {
+                arr.add(element);
             }
         }
-        System.out.println("send = " + send);
+        String send = "";
+        for(int i = 0; i < arr.size(); i++) {
+            if((i == 0 || arr.get(i - 1).isEmpty()) && arr.get(i).equals(word)) {
+                send += arr.get(i + 1).charAt(0);
+            } else if (i == arr.size() - 1 && arr.get(i).equals(word)) {
+                send += arr.get(i - 1).charAt(arr.get(i - 1).length() - 1);
+            } else if (arr.get(i).equals(word)) {
+                send += arr.get(i - 1).charAt(arr.get(i - 1).length() - 1);
+                send += arr.get(i + 1).charAt(0);
+            }
+        }
+        return send;
     }
 }
