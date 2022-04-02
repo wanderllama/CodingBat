6bf6bbc0a4eb19fcadc7d7a15cd56b452b7277f8
James Wallace <jawallace0392@gmail.com>
2022-04-02 07:44:19 -0400

    not finished need to switch to using while loop

--- src/String3/WithoutString.java
+++ src/String3/WithoutString.java
@@ -8,37 +8,34 @@ public class WithoutString {
 
     public static void main(String[] args) {
 
-    String base = "THIS is a FISH", remove = "is";
-    String send = "";
 
-        for(
-    int i = 0;
-    i <=base.length()-remove.length();i++)
+        String base = "THIS is a FISH", remove = "is";
+        String send = "";
 
-    {
-        int j = 0;
-        System.out.println(remove.charAt(j));
-        if (("" + base.charAt(i)).equalsIgnoreCase("" + remove.charAt(j))) {
-            int start = i;
-            int end = start;
-            i++;
-            j++;
-            //using i variable later cause letters to be skipped when for loop restarts
-            //introduce new variable that will start at i and compare the right letters to j along the way
-            //increment i to no repeat letters that are checked in while loop
-            while (j < remove.length()) {
-                if (("" + base.charAt(i++)).equalsIgnoreCase("" + remove.charAt(j))) {
-                    end++;
-                    j++;
-                } else {
-                    break;
+        for (int i = 0; i <= base.length() - remove.length(); i++) {
+            int j = 0;
+            System.out.println(remove.charAt(j));
+            if(("" + base.charAt(i)).equalsIgnoreCase("" + remove.charAt(j))) {
+                int start = i;
+                int end = start;
+                j++;
+                int x = i + 1;
+                //using i variable later cause letters to be skipped when for loop restarts
+                //introduce new variable that will start at i and compare the right letters to j along the way
+                //increment i to no repeat letters that are checked in while loop
+                while (j < remove.length()) {
+                    if (("" + base.charAt(x++)).equalsIgnoreCase("" + remove.charAt(j))) {
+                        end++;
+                        j++;
+                    } else {
+                        break;
+                    }
+                }
+                if (j == remove.length()) {
+                    base = base.substring(0 , start) + base.substring(end + 1);
+                    System.out.println(base);///
                 }
             }
-            if (j == remove.length()) {
-                base = base.substring(0, start) + base.substring(end + 1);
-                System.out.println(base);///
-            }
         }
-    }
 }
 }
