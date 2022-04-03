70f18750be8efa31d5e142193ed98a9529c3b60f
James Wallace <jawallace0392@gmail.com>
2022-04-03 10:15:06 -0400

    not finished case needed for single letter remove string and overlapping remove string in base

--- src/String3/WithoutString.java
+++ src/String3/WithoutString.java
@@ -4,38 +4,41 @@ package String3;
 remove string have been removed (not case sensitive). You may assume that the remove string is
 length 1 or more. Remove only non-overlapping instances, so with "xxx" removing "xx" leaves "x".*/
 
+import java.util.ArrayList;
+
 public class WithoutString {
 
     public static void main(String[] args) {
+        String base = "FISh is THiS";
+        String remove = "is";
 
-
-        String base = "THIS is a FISH", remove = "is";
-        String send = "";
-
+        ArrayList<Integer> list = new ArrayList<>();
         for (int i = 0; i <= base.length() - remove.length(); i++) {
-            int j = 0;
-            System.out.println(remove.charAt(j));
-            if(("" + base.charAt(i)).equalsIgnoreCase("" + remove.charAt(j))) {
-                int start = i;
-                int end = start;
-                j++;
-                int x = i + 1;
-                //using i variable later cause letters to be skipped when for loop restarts
-                //introduce new variable that will start at i and compare the right letters to j along the way
-                //increment i to no repeat letters that are checked in while loop
-                while (j < remove.length()) {
-                    if (("" + base.charAt(x++)).equalsIgnoreCase("" + remove.charAt(j))) {
-                        end++;
-                        j++;
-                    } else {
-                        break;
+            if (("" + base.charAt(i)).equalsIgnoreCase("" + remove.charAt(0))) {
+                int count = 1;
+                if (remove.length() > 1) {
+                    for (int j = i + 1 , k = 1; k < remove.length(); j++ , k++) {
+                        if (("" + base.charAt(j)).equalsIgnoreCase("" + remove.charAt(k))) {
+                            count++;
+                        }
                     }
-                }
-                if (j == remove.length()) {
-                    base = base.substring(0 , start) + base.substring(end + 1);
-                    System.out.println(base);///
+                    if (count == remove.length()) {
+                        int indexAdd = i;
+                        while (count > 0) {
+                            list.add(indexAdd);
+                            indexAdd++;
+                            count--;
+                        }
+                    }
                 }
             }
         }
-}
+        String send = "";
+        for (int i = 0; i < base.length(); i++) {
+            if (!list.contains(i)) {
+                send += base.charAt(i);
+            }
+        }
+        System.out.println("list = " + list);
+    }
 }
