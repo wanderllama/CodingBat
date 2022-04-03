3e103cb6e85df7f0d62e9e4642271fea5c5a4318
James Wallace <jawallace0392@gmail.com>
2022-04-03 14:18:46 -0400

    finished

---
+++ src/String3/SameEnds.java
@@ -0,0 +1,17 @@
+package String3;
+
+/*Given a string, return the longest substring that appears at both the beginning
+and end of the string without overlapping. For example, sameEnds("abXab") is "ab".*/
+
+public class SameEnds {
+    public String sameEnds(String string) {
+        String send = "";
+        for(int i = string.length() / 2; i > 0; i--) {
+            if(string.substring(0 , i).equals(string.substring(string.length() - i))) {
+                send = string.substring(0 , i);
+                break;
+            }
+        }
+        return send;
+    }
+}
