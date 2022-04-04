a4e61e0a2bbb7b1844c692b4851dec0dcd9d8863
James Wallace <jawallace0392@gmail.com>
2022-04-04 15:26:12 -0400

    finished -> star

--- src/array2/CenteredAverage.java
+++ src/array2/CenteredAverage.java
@@ -3,28 +3,29 @@ package array2;
 /*Return the "centered" average of an array of ints, which we'll say is the mean average of the values, except ignoring the largest and smallest values in the array. If there are multiple copies of the smallest value, ignore just one copy, and likewise for the largest value. Use int division to produce the final average. You may assume that the array is length 3 or more.*/
 
 
-import java.util.Arrays;
-
 public class CenteredAverage {
-    public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4, 100};
+    public int centeredAverage(int[] nums) {
+        int max = nums[0];
+        int min = nums[0];
+        int[] index = {0, 0};
         int sum = 0;
-        int[] maxMin = {nums[0] , nums[1]};
-        for(int i = 0; i < nums.length; i++) {
-            if(nums[i] > maxMin[0]) {
-                maxMin[0] = i;
-            } else if(nums[i] < maxMin[1]) {
-                maxMin[1] = i;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < min) {
+                min = nums[i];
+                index[0] = i;
+            } else if (nums[i] > max) {
+                max = nums[i];
+                index[1] = i;
             }
         }
-        System.out.println(Arrays.toString(maxMin));
-        for(int i = 0; i < nums.length; i++) {
-            if(i == maxMin[0] || i == maxMin[1]) {
-                continue;
-            } else {
+        if(min == max) {
+            return min;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (i != index[0] && i != index[1]) {
                 sum += nums[i];
             }
         }
-        System.out.println(sum/(nums.length - 2));
+        return sum / (nums.length - 2);
     }
 }
