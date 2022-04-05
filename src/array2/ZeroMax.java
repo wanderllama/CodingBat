f217f85b0b798382cea3f9f45624fed7fef25076
James Wallace <jawallace0392@gmail.com>
2022-04-04 22:33:47 -0400

    finished but not working in codingBat??? passes all test byt the test cast in file this one prints 7,1,1,7,7,7 and it should be 7,7,1,7,7,7 according to coding bat but i disagree

--- src/array2/ZeroMax.java
+++ src/array2/ZeroMax.java
@@ -1,25 +1,24 @@
 package array2;
 
 /*Return a version of the given array where each zero value in the array is replaced by the largest odd value to the right of the zero in the array. If there is no odd value to the right of the zero, leave the zero as a zero.*/
-
 import java.util.Arrays;
 
 public class ZeroMax {
     public static void main(String[] args) {
 
-        int[] nums = {7,3,4,3,0,1};
-
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
+        int[] nums = {7,0,1,0,0,7};
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == 0) {
                 int odd = -1;
-                while (++i < nums.length && nums[i] != 0) {
-                    if (nums[i] % 2 == 1 && nums[i] > odd) {
+                while(++i < nums.length && nums[i] != 0) {
+                    if(nums[i] % 2 == 1 && nums[i] > odd) {
                         odd = nums[i];
                     }
                 }
-                if (odd != -1) {
-                    for (int j = 0; j < i; j++) {
-                        if (nums[j] == 0) {
+                i--;
+                if(odd != -1) {
+                    for(int j = 0; j < i + 1; j++) {
+                        if(nums[j] == 0) {
                             nums[j] = odd;
                         }
                     }
