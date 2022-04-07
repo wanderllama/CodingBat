33e381edcad0e8811b60c8c21d8bd261d85f94fc
James Wallace <jawallace0392@gmail.com>
2022-04-07 10:50:34 -0400

    finished

---
+++ src/ap1/MatchUp.java
@@ -0,0 +1,18 @@
+package ap1;
+
+/*Given 2 arrays that are the same length containing strings, compare the 1st string in one array to the 1st string in the other array, the 2nd to the 2nd and so on. Count the number of times that the 2 strings are non-empty and start with the same char. The strings may be any length, including 0.*/
+
+public class MatchUp {
+    public int matchUp(String[] a, String[] b) {
+        int count = 0;
+        for(int i = 0; i < a.length; i++) {
+            if(a[i].isEmpty() || b[i].isEmpty()) {
+                continue;
+            }
+            if(a[i].charAt(0) == b[i].charAt(0)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
