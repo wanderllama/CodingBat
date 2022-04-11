727a006384ffa30fb3ed9a614afbf709ee729576
James Wallace <jawallace0392@gmail.com>
2022-04-11 08:53:00 -0400

    finished return outside of if statement is the stop return and the method will run recursively until if statement is false

--- src/recursion1/Factorial.java
+++ src/recursion1/Factorial.java
@@ -5,15 +5,16 @@ package recursion1;
 public class Factorial {
     public int factorial(int n) {
         int n2 = n;
-        if(n < 2) {
+        if (n < 2) {
             return n;
         }
         return n * factorial(n - 1);
     }
-
+}
+/*
     //alternate -> switching the if statement to be the reverse and the return outside of the if statement is the stop condition'
         if(n2 > 1) {
             return fibonacci(n2 - 1) + fibonacci(n2 - 2);
         }
         return n2;
-    }
+    }*/
