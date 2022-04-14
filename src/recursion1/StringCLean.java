fc0015d97c21046016ecf32b58f00ed5a040bfb1
James Wallace <jawallace0392@gmail.com>
2022-04-14 08:48:25 -0400

    finished less complicated than i was trying to make it

--- src/recursion1/StringCLean.java
+++ src/recursion1/StringCLean.java
@@ -12,20 +12,9 @@ public class StringCLean {
                 return "" + (str.charAt(0));
             }
         }
-        int index = 1;
-        for(int i = 0; i < str.length(); i++) {
-            if(str.charAt(i) == str.charAt(i + 1)) {
-                index++;
-                for(int j = i + 1; j < str.length(); j++) {
-                    if(str.charAt(j) == str.charAt(j + 1)) {
-                        index++;
-                    } else {
-                        break;
-                    }
-                }
-            }
-            break;
+        if(str.charAt(0) == str.charAt(1)) {
+            return stringClean(str.substring(1));
         }
-        return str.charAt(0) + stringClean(str.substring(index));
+        return str.charAt(0) + stringClean(str.substring(1));
     }
 }
