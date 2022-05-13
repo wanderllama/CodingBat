830ec49450bed9a2c0c7d1461a770b24edc1c723
James Wallace <jawallace0392@gmail.com>
2022-05-13 19:52:28 -0400

    update -> more simple and efficient

--- src/string2/StarOut.java
+++ src/string2/StarOut.java
@@ -5,7 +5,33 @@ and the chars immediately to its left and right are gone. So "ab*cd" yields "ad"
 also yields "ad".*/
 
 public class StarOut {
+
+    // second approach
     public String starOut(String str) {
+    int[] arr = new int[str.length()];
+    String send = "";
+  for(int i = 0; i < arr.length; i++) {
+        if(str.charAt(i) == '*') {
+            arr[i]++;
+            if(i == 0) {
+                arr[i + 1]++;
+                continue;
+            } else if (i == str.length() - 1) {
+                arr[i - 1]++;
+                continue;
+            }
+            arr[i - 1]++;
+            arr[i + 1]++;
+        }
+    }
+  for(int i = 0; i < arr.length; i++) {
+        if(arr[i] == 0) {
+            send += "" + str.charAt(i);
+        }
+    }
+  return send;
+}
+  /*  public String starOut(String str) {
         String send = "";
         for (int i = 0; i < str.length(); i++) {
             if (str.charAt(i) == '*') {
@@ -34,5 +60,5 @@ public class StarOut {
             }
         }
         return send;
-    }
+    }*/
 }
