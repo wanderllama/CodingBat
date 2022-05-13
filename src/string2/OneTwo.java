4ec19b89170df0b10f826db7569eb6852c6c1eab
James Wallace <jawallace0392@gmail.com>
2022-05-13 19:39:40 -0400

    update -> more simple and efficient

--- src/string2/OneTwo.java
+++ src/string2/OneTwo.java
@@ -5,7 +5,7 @@ the next two chars, so "abc" yields "bca". Repeat this process for each subseque
 group of 3 chars, so "abcdef" yields "bcaefd". Ignore any group of fewer than 3 chars at the end.*/
 
 public class OneTwo {
-    public String oneTwo(String str) {
+/*    public String oneTwo(String str) {
         if (str.length() < 3) {
             return "";
         }
@@ -22,6 +22,19 @@ public class OneTwo {
         String send = "";
         for(int i = 0; i < strs.length; i++) {
             send += strs[i].substring(1) + strs[i].charAt(0);
+        }
+        return send;
+    }*/
+
+    //second approach
+    public String oneTwo(String str) {
+        if (str.length() < 3) {
+            return "";
+        }
+        String send = "";
+        while(str.length() >= 3) {
+            send += str.substring(1 , 3) + str.charAt(0);
+            str = str.substring(3);
         }
         return send;
     }
